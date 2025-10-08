e28ee3920c972ecf65ee3489b6303c0c0ce340ee
Arantxa Angulo <akrankaa10@gmail.com>
2025-10-08 17:48:12 -0600

    Whitelist update

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -9,7 +9,7 @@ const ClassSession = require('../schemas/classSessions.model');
 const Package = require('../schemas/packages.model');
 
 // ADMIN WHITELIST
-const ADMIN_EMAILS = ['wavesfw@gmail.com', 'violettquezada15@gmail.com']; 
+const ADMIN_EMAILS = ['wavesfw@gmail.com', 'violettquezada15@gmail.com', 'adminv_rebettx1@gmail.com']; 
 
 // Custom admin verification middleware
 async function verifyAdmin(req, res, next) {
